8bf884834d1b644fb4ab9e84f5e0ba12ed8e8985
Marcel <henryluchs@gmail.com>
2023-12-05 14:47:27 +0100

    solved day 1 puzzle

--- day1/main.py
+++ day1/main.py
@@ -4,9 +4,25 @@ import re
 with open("aufgabe.txt", 'r') as str1:
     str1 = str1.readlines()
 
-# print(re.sub("\D", "", str1))
+
+# string replace
+
+original_string = [string for string in str1]
+replacements = {'one': 'o1e', 'two': 't2o', 'three': 'th3ee', 'four': 'f4ur', 'five': 'fi5ve', 'six': 's6x', 'seven': 'se7en', 'eight': 'ei8ght', 'nine': 'ni9ne', 'zero': 'ze0ro'  }
+
+for old, new in replacements.items():
+    original_string = [re.sub(old, new, string) for string in original_string]
+
+# ende string replace
 
 
-filtered_lines = [re.sub("\D", "", line) for line in str1]
-for filtered_line in filtered_lines:
-    print(filtered_line)
+
+total = 0
+for line in original_string:
+    digits = ''.join(filter(str.isdigit, line))
+    if digits:
+        filtered_line = (int(digits[0] + digits[-1]))
+        total += filtered_line
+        
+        
+print("Total is: ", total)
